e8a6ef20ad610bf385396ed481ea386d5d71afa3
RoboticsRafsan01 <mzaman1@stuy.edu>
2018-11-24 14:35:41 -0500

    Comments added!

--- MyString.java
+++ MyString.java
@@ -1,6 +1,8 @@
 public class MyString implements Charsequence,Comparable<Charsequence>{
+// Field(s)
   private char[] data;
 
+// Constructor
   public MyString(Charsequence s) {
     data = new char[s.length()];
     for(int i = 0; i < s.length(); i++) {
@@ -8,12 +10,14 @@ public class MyString implements Charsequence,Comparable<Charsequence>{
     }
   }
 
+// Comparable method required
   public int compareTo(MyString object) {
 
   }
 
   // Methods that must be written
 
+// Returns the length of the Charsequence
   public int length() {
     if(index < 0 || index >= data.length) {
       throw new IndexOutOfBoundsException();
@@ -21,10 +25,12 @@ public class MyString implements Charsequence,Comparable<Charsequence>{
     return this.data.length;
   }
 
+// Returns the char at a specific index
   public char charAt(int index) {
     return data[index];
   }
 
+// Returns a part of the Charsequence, much like substring
   public Charsequence subSequence(int start, int end) {
     String result = "";
     for(int i = start; i < end; i++) {
@@ -33,6 +39,7 @@ public class MyString implements Charsequence,Comparable<Charsequence>{
     return result;
   }
 
+// Standard toString method
   public String toString{
     String result = "";
     for(int i = 0; i < length(); i++ {
